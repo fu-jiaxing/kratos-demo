6d33100fb6b7b2dfd13185d85a2e8d682d998aa6
fu-jiaxing <fujiaxing.zero@gmail.com>
2023-02-28 22:00:49 +0800

    client script

--- user_client/cmd/main.go
+++ user_client/cmd/main.go
@@ -1,1 +1,76 @@
-package cmd
+package main
+
+import (
+	"context"
+	"github.com/go-kratos/kratos/contrib/registry/consul/v2"
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/selector"
+	"github.com/go-kratos/kratos/v2/selector/wrr"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	consul_api "github.com/hashicorp/consul/api"
+	"os"
+	"time"
+	user_info "user_client/api"
+)
+
+var (
+	Name = "bbs.user.client"
+)
+
+func callRPC(client user_info.UserInfoClient) {
+	reply, err := client.GetUserInfo(context.Background(), &user_info.GetUserInfoRequest{UserID: 1})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Infof("[grpc] GetUserInfo %v\n", reply)
+}
+
+func main() {
+	consulCli, err := consul_api.NewClient(consul_api.DefaultConfig())
+
+	if err != nil {
+		panic(err)
+	}
+
+	r := consul.New(consulCli)
+	selector.SetGlobalSelector(wrr.NewBuilder())
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint("discovery:///bbs.user.info"),
+		grpc.WithDiscovery(r),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	gClient := user_info.NewUserInfoClient(conn)
+	ch := make(chan bool)
+
+	go func(client user_info.UserInfoClient, done chan bool) {
+		for {
+			select {
+			case <-done:
+				return
+			default:
+				time.Sleep(time.Second)
+				callRPC(client)
+			}
+		}
+	}(gClient, ch)
+
+	id, _ := os.Hostname()
+
+	client, err := consul_api.NewClient(consul_api.DefaultConfig())
+
+	app := kratos.New(
+		kratos.ID(id),
+		kratos.Name(Name),
+		kratos.Registrar(consul.New(client)),
+	)
+	if err := app.Run(); err != nil {
+		ch <- true
+		log.Fatal(err)
+	}
+}
---
+++ user_client/go.mod
@@ -0,0 +1,34 @@
+module user_client
+
+go 1.19
+
+require (
+	github.com/go-kratos/kratos/contrib/registry/consul/v2 v2.0.0-20230227131608-c65f823c38de
+	github.com/go-kratos/kratos/v2 v2.5.4
+	github.com/hashicorp/consul/api v1.19.1
+	google.golang.org/genproto v0.0.0-20230227214838-9b19f0bdc514
+	google.golang.org/grpc v1.53.0
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	github.com/armon/go-metrics v0.3.10 // indirect
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+	github.com/hashicorp/go-hclog v0.14.1 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.0 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/serf v0.10.1 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
+)
