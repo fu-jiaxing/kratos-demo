517e946ff504b53dbdc646e2996900b8ffc8efe0
fu-jiaxing <fujiaxing.zero@gmail.com>
2023-03-01 21:34:50 +0800

    add breaker expr

--- user_client/cmd/main.go
+++ user_client/cmd/main.go
@@ -2,32 +2,52 @@ package main
 
 import (
 	"context"
+	prom "github.com/go-kratos/kratos/contrib/metrics/prometheus/v2"
 	"github.com/go-kratos/kratos/contrib/registry/consul/v2"
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/circuitbreaker"
+	"github.com/go-kratos/kratos/v2/middleware/metrics"
 	"github.com/go-kratos/kratos/v2/selector"
 	"github.com/go-kratos/kratos/v2/selector/wrr"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
-	consul_api "github.com/hashicorp/consul/api"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	consulapi "github.com/hashicorp/consul/api"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"os"
-	"time"
-	user_info "user_client/api"
+	userinfo "user_client/api"
 )
 
 var (
 	Name = "bbs.user.client"
 )
 
-func callRPC(client user_info.UserInfoClient) {
-	reply, err := client.GetUserInfo(context.Background(), &user_info.GetUserInfoRequest{UserID: 1})
+var (
+	_metricClientQPS = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace:   "bbs_user_client",
+		Subsystem:   "requests",
+		Name:        "throughput",
+		Help:        "throughput",
+		ConstLabels: nil,
+	}, []string{"kind", "operation", "code", "reason"})
+)
+
+func init() {
+	prometheus.MustRegister(_metricClientQPS)
+}
+
+func callRPC(client userinfo.UserInfoClient) {
+	reply, err := client.GetUserInfo(context.Background(), &userinfo.GetUserInfoRequest{UserID: 1})
 	if err != nil {
-		log.Fatal(err)
+		log.Errorf("[grpc] GetUserInfo failed: ", err)
+	} else {
+		log.Infof("[grpc] GetUserInfo %v\n", reply)
 	}
-	log.Infof("[grpc] GetUserInfo %v\n", reply)
 }
 
 func main() {
-	consulCli, err := consul_api.NewClient(consul_api.DefaultConfig())
+	consulCli, err := consulapi.NewClient(consulapi.DefaultConfig())
 
 	if err != nil {
 		panic(err)
@@ -39,35 +59,46 @@ func main() {
 		context.Background(),
 		grpc.WithEndpoint("discovery:///bbs.user.info"),
 		grpc.WithDiscovery(r),
+		grpc.WithMiddleware(
+			metrics.Client(metrics.WithRequests(prom.NewCounter(_metricClientQPS))),
+			circuitbreaker.Client(),
+			// metrics.Client(metrics.WithRequests(prom.NewCounter(_metricClientQPS))),
+		),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
-	gClient := user_info.NewUserInfoClient(conn)
+	gClient := userinfo.NewUserInfoClient(conn)
 	ch := make(chan bool)
 
-	go func(client user_info.UserInfoClient, done chan bool) {
+	go func(client userinfo.UserInfoClient, done chan bool) {
 		for {
 			select {
 			case <-done:
 				return
 			default:
-				time.Sleep(time.Second)
+				// time.Sleep(time.Duration(rand.Intn(1000)))
 				callRPC(client)
 			}
 		}
 	}(gClient, ch)
 
+	httpSrv := http.NewServer(
+		http.Address(":8100"),
+	)
+	httpSrv.Handle("/metrics", promhttp.Handler())
+
 	id, _ := os.Hostname()
 
-	client, err := consul_api.NewClient(consul_api.DefaultConfig())
+	client, err := consulapi.NewClient(consulapi.DefaultConfig())
 
 	app := kratos.New(
 		kratos.ID(id),
 		kratos.Name(Name),
 		kratos.Registrar(consul.New(client)),
+		kratos.Server(httpSrv),
 	)
 	if err := app.Run(); err != nil {
 		ch <- true
--- user_client/go.mod
+++ user_client/go.mod
@@ -3,9 +3,11 @@ module user_client
 go 1.19
 
 require (
+	github.com/go-kratos/kratos/contrib/metrics/prometheus/v2 v2.0.0-20230227131608-c65f823c38de
 	github.com/go-kratos/kratos/contrib/registry/consul/v2 v2.0.0-20230227131608-c65f823c38de
 	github.com/go-kratos/kratos/v2 v2.5.4
 	github.com/hashicorp/consul/api v1.19.1
+	github.com/prometheus/client_golang v1.14.0
 	google.golang.org/genproto v0.0.0-20230227214838-9b19f0bdc514
 	google.golang.org/grpc v1.53.0
 	google.golang.org/protobuf v1.28.1
@@ -13,9 +15,12 @@ require (
 
 require (
 	github.com/armon/go-metrics v0.3.10 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/hashicorp/go-hclog v0.14.1 // indirect
@@ -23,12 +28,20 @@ require (
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/serf v0.10.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.40.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
