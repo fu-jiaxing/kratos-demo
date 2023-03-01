01b6d0279cd28807c2edac1d71b81b0420137b4d
fu-jiaxing <fujiaxing.zero@gmail.com>
2023-03-01 21:34:13 +0800

    add breaker expr

--- user_info/internal/server/grpc.go
+++ user_info/internal/server/grpc.go
@@ -1,15 +1,17 @@
 package server
 
 import (
+	"context"
 	ratelimit2 "github.com/go-kratos/aegis/ratelimit"
 	prom "github.com/go-kratos/kratos/contrib/metrics/prometheus/v2"
+	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware"
 	"github.com/go-kratos/kratos/v2/middleware/metrics"
-	"github.com/go-kratos/kratos/v2/middleware/ratelimit"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"golang.org/x/time/rate"
-	"time"
+	"math/rand"
 	pb "user_info/api/user_info"
 	"user_info/conf"
 	"user_info/internal/service"
@@ -22,21 +24,32 @@ type Limiter struct {
 
 func (l *Limiter) Allow() (ratelimit2.DoneFunc, error) {
 	if !l.limiter.Allow() {
-		return nil, ratelimit2.ErrLimitExceed
+		return nil, errors.New(504, "rate limit exceeded", "testsettetsetstst")
 	}
 	return func(info ratelimit2.DoneInfo) {
 
 	}, nil
 }
 
+func RandomError500(handler middleware.Handler) middleware.Handler {
+	return func(ctx context.Context, req interface{}) (interface{}, error) {
+		if rand.Intn(10) < 5 {
+			return nil, errors.New(500, "random error", "random error")
+		}
+		reply, err := handler(ctx, req)
+		return reply, err
+	}
+}
+
 // NewGRPCServer new a gRPC server.
 func NewGRPCServer(c *conf.Server, service *service.UserInfoService, logger log.Logger) *grpc.Server {
-	tokenLimiter := Limiter{limiter: rate.NewLimiter(rate.Every(500*time.Microsecond), 1)}
+	// tokenLimiter := Limiter{limiter: rate.NewLimiter(rate.Every(100*time.Millisecond), 1)}
 
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
-			ratelimit.Server(ratelimit.WithLimiter(&tokenLimiter)),
+			// ratelimit.Server(ratelimit.WithLimiter(&tokenLimiter)),
 			recovery.Recovery(),
+			// RandomError500,
 			metrics.Server(
 				metrics.WithSeconds(prom.NewHistogram(_metricServerSeconds)),
 				metrics.WithRequests(prom.NewCounter(_metricServerQPS)),
